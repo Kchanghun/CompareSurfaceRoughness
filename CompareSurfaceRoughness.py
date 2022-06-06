7603f1a24caac991c31cdea0350d563e42c42b0e
Kchanghun <abcd877287@gmail.com>
2022-06-07 04:59:22 +0900

    import library

--- CompareSurfaceRoughness.py
+++ CompareSurfaceRoughness.py
@@ -1,8 +1,46 @@
+import io
 import cv2
+import base64 
+import glob
+import os
 import numpy as np
-from matplotlib import pyplot as plt
+from PIL import Image
 
-img = cv2.imread('./DataImage/wood_texture1.png')
-cv2.imshow('temp',img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+datapath = './DataImage/*'
+
+# from image to base64
+# img = cv2.imread(filepath)
+# jpg_img = cv2.imencode('.jpg',img)
+# b64_string = base64.b64encode(jpg_img[1]).decode('utf-8')
+# print(jpg_img)
+
+# # from base64 to image
+# imgdata = base64.b64decode(b64_string)
+# dataBytesIO = io.BytesIO(imgdata)
+# image = Image.open(dataBytesIO)
+
+# camera image size = (4032,3024)
+# print(np.array(image).shape)
+
+# resize image
+files = glob.glob(datapath)
+for f in files:
+    try:
+        img = Image.open(f)
+        img_resize = img.resize((256,256))
+        LAN_img_resize = img.resize((256,256),Image.LANCZOS)
+        title,ext = os.path.splitext(f)
+        img_resize.save(title+ext)
+        LAN_img_resize.save(title+'asdf'+ext)
+        print(title)
+    except:
+        print('eror')
+        pass
+
+
+
+
+# show image
+# cv2.imshow('이게됨?',cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB))
+# cv2.waitKey()
+# cv2.destroyAllWindows()
